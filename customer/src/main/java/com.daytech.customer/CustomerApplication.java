13086b37eec2eff18060ffe0ad814104ad7b8247
DjairCosta <riajd10@gmail.com>
2022-05-25 06:35:07 -0300

    First commit microservices

---
+++ customer/src/main/java/com.daytech.customer/CustomerApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class CustomerApplication {
+
+    @SpringBootApplication
+    public static void main(String[] args) {
+        SpringBootApplication.run(CustomerApplication.class, args);
+    }
+
+}
